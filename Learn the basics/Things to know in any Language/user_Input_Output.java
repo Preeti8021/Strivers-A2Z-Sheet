3be52583b8721a2201235a907d592c7cbc04406d
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-02-02 23:14:09 +0530

    Create user_Input_Output.java

---
+++ Learn the basics/Things to know in any Language/user_Input_Output.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if(Character.isUpperCase(c)){
+            System.out.println(1);
+        }
+        else if(Character.isLowerCase(c)){
+            System.out.print(0);
+        }
+        else{
+            System.out.print(-1);
+        }
+
+    }
+}
