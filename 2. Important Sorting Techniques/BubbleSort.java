07a51e149d5ffebb0f5970a374236062426c9745
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-06-08 16:53:37 +0530

    Update BubbleSort.java

--- 2. Important Sorting Techniques/BubbleSort.java
+++ 2. Important Sorting Techniques/BubbleSort.java
@@ -1,26 +1,32 @@
 //Logic: Push the max to the end with adjacent swaps 
-//Time complexity: O(n2)
+//Time complexity(worst, average) : O(n2)
+//Time complexity(best case): O(n) - if already the array is sorted then no need to check after first iteration of checking the entire array for swaps, if no swap, then break out from loop 
 
 public class BubbleSort
 {
-  public static void main(String[] args){
-    int[] arr = {5,2,6,8,9};
-    int n = 5; 
-    bubbleSort(arr,n);
-    for(int i = 0; i<n; i++){
-      System.out.print(arr[i]+" "); 
-    }
-  }
+	public static void main(String[] args){
+		int[] arr = {5,2,6,8,9};
+		int n = 5; 
+		bubbleSort(arr,n);
+		for(int i = 0; i<n; i++){
+			System.out.print(arr[i]+" "); 
+		}
+	}
 	public static void bubbleSort(int arr[], int n)
-    {
-        for(int i = n-1; i>=1; i--){
-            for(int j = 0; j<i; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]= arr[j+1];
-                    arr[j+1]= temp; 
-                }
-            }
-        }
-    }
+	{
+		//boolean noswap = true; 
+		for(int i = n-1; i>=1; i--){
+			for(int j = 0; j<i; j++){
+				if(arr[j]>arr[j+1]){
+					int temp = arr[j];
+					arr[j]= arr[j+1];
+					arr[j+1]= temp; 
+					//noswap = false; 
+				}
+			}
+			// if(noswap){
+			// 	break;
+			// }
+		}
+	}
 }
