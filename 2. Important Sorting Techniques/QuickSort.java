b5afb2c42f373a31b0fc7912a1e779db0dcfbfb7
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-06-08 21:42:06 +0530

    Create QuickSort.java

---
+++ 2. Important Sorting Techniques/QuickSort.java
@@ -0,0 +1,6 @@
+//Logic: 
+//Pick a pivot and place it in correct place (pivot can first,last,middle or random element consistent throughout algorithm)
+//Smaller element on left, larger on right
+//Time Complexity: O(nlogn)
+//Space Complexity: O(1)
+
