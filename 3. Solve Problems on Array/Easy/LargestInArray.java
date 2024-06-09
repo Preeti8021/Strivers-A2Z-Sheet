1a4b65189f29c3e9c1a2b141a04d4451252037db
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-06-09 16:24:42 +0530

    Update LargestInArray.java

--- 3. Solve Problems on Array/Easy/LargestInArray.java
+++ 3. Solve Problems on Array/Easy/LargestInArray.java
@@ -1,10 +1,11 @@
+//TC - O(n)
 public class LargestInArray {
     public static void main(String[] args){
       int[] arr = {1, 8, 7, 56, 90};
       int n = 5; 
       System.out.print(largest(arr,n)); 
     }
-    public int largest(int arr[], int n)
+    public static int largest(int arr[], int n)
     {
         int max = arr[0];
         for(int i = 0; i<n; i++){
