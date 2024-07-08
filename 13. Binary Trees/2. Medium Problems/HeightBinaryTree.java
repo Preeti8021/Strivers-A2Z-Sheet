3dade5f168a5e40ae9230159addeee2b0aab66e8
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-07-08 10:01:06 +0530

    add height of binary tree

---
+++ 13. Binary Trees/2. Medium Problems/HeightBinaryTree.java
@@ -0,0 +1,37 @@
+//Logic: Same like level order, just create a variable called height and increment whenever go to next level
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int depth = 0; 
+        Queue<TreeNode> q = new LinkedList<TreeNode>(); 
+        if(root == null){
+            return 0; 
+        }
+        q.offer(root);
+        while(!q.isEmpty()){
+            int levelSize = q.size(); 
+            for(int i = 0; i<levelSize; i++){
+                if(q.peek().left!=null){
+                    q.add(q.peek().left);
+                }
+                if(q.peek().right!=null){
+                    q.add(q.peek().right);
+                }
+                q.poll();
+            }
+            depth+=1; 
+        }
+        return depth;
+    }
+}
