97ba0587d70093cb9e589e4c4938c42827fd73a1
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-07-26 12:41:51 +0530

    add alternate method

--- 05. Strings/Medium String Problems/ReverseWordsInString.java
+++ 05. Strings/Medium String Problems/ReverseWordsInString.java
@@ -1,6 +1,28 @@
 //Challenge - remove the extra spaces 
 
-public class ReverseWordsInString {
+//Logic- used stack to store the strings, from string array containing words from given string
+class ReverseWordsInString1 {
+    public String reverseWords(String s) {
+        Stack<String> words = new Stack<>(); 
+        String[] arr = s.split(" ");
+        for(int i = 0; i<arr.length; i++){
+            if(!arr[i].isEmpty()){
+                words.push(arr[i]);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        while(!words.isEmpty()){
+            res.append(words.pop());
+            if(!words.isEmpty()) res.append(" ");
+        }
+
+        return res.toString();
+    }
+}
+
+//Logic- split function with delimiter 
+class ReverseWordsInString2 {
     public String reverseWords(String s) {
         String[] arr = s.trim().split("\\s+");
         StringBuilder sb = new StringBuilder();
@@ -15,5 +37,3 @@ public class ReverseWordsInString {
         return sb.toString();
     }
 }
-
-//TODO - other methods to remove the spaces (alternative to split("\\s+"))
