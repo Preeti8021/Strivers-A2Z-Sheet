d1775e3d414bc4ccde8e644b1f1c237bba0fd2b9
Preeti <103981488+Preeti8021@users.noreply.github.com>
2024-08-20 17:24:51 +0530

    add is string rotation

---
+++ 05. Strings/Basic and Easy on Strings/IsStringRotation.java
@@ -0,0 +1,21 @@
+//logic: concatentate goal with itself, if that result contains the string to be checked, then 
+//       both strings are rotation of each other 
+//TC: O(m*n) SC: O(m)  
+//{m - length of goal, n - length of s} 
+
+
+public class IsStringRotation {
+    public boolean rotateString(String s, String goal) {
+        if(s.equals(goal)){
+            return true;
+        }
+        if(s.length()>goal.length()){
+            return false;
+        }
+        String news = goal + goal;
+        if(news.contains(s)){
+            return true;
+        }
+        return false;
+    }
+}
