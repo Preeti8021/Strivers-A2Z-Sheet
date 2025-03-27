5f174784fd0a737a66432bc3d4307a6276270f7e
Preeti <103981488+Preeti8021@users.noreply.github.com>
2025-03-27 12:04:18 +0530

    Update ReverseDLL.java

--- 06. LinkedList/02. Learn Doubly LinkedList/ReverseDLL.java
+++ 06. LinkedList/02. Learn Doubly LinkedList/ReverseDLL.java
@@ -29,3 +29,24 @@ class ReverseDLL {
         return left.prev; 
     }
 }
+
+
+/* no additional checks before*/
+class ReverseDLL2 {
+    public DLLNode reverseDLL(DLLNode head) {
+        DLLNode curr = head; 
+        DLLNode left = null; 
+        
+        while(curr!=null){
+            left = curr.prev; 
+            curr.prev = curr.next; 
+            curr.next = left; 
+            
+            curr = curr.prev; 
+        }
+        if(left!=null){
+            return left.prev; 
+        }
+        return head; 
+    }
+}
